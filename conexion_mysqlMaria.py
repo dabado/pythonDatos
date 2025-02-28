c7360dd302b00429d281970bf5704eab52ecf8c8
dabado <davidbarroso@telefonica.net>
2025-02-28 20:52:04 +0100

    dbd

---
+++ conexion_mysqlMaria.py
@@ -0,0 +1,16 @@
+import pymysql 
+
+conection = pymysql.connect( host='localhost', port=3306, user='getafe', password='mysql', database='HOSPITAL')
+if conection:
+    print(f".....conexion correcta {conection}")
+else:
+    print("fallo de conexion")
+sql="select APELLIDO FROM EMP"
+
+cursor= conection.cursor()
+cursor.execute(sql)
+datos = []
+for row in cursor:
+            
+    datos.append(row)
+print(f"resultados:   {datos}")
---
+++ conexion_sqlServeer.py
@@ -0,0 +1,12 @@
+import pyodbc
+
+conection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=HOSPITAL;UID=SA;password=Getafe12345@@;TrustServerCertificate=yes')
+print("funciona")
+sql="select APELLIDO from EMP"
+cursor= conection.cursor()
+cursor.execute(sql)
+datos = []
+for row in cursor:
+            
+    datos.append(row)
+print(datos)
