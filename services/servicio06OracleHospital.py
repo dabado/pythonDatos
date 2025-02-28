f7a7031466ec7513819990071cb01a7c32d96502
DavidB <davidbarroso@telefonica.net>
2025-02-28 13:15:08 +0100

    Update servicio06OracleHospital.py

--- services/servicio06OracleHospital.py
+++ services/servicio06OracleHospital.py
@@ -12,12 +12,14 @@ class ServiceOracleHospital:
         else:
             print("fallo de conexion")
 
-    def muestraHospital(self):
+    def muestraHospitales(self):
         sql="select * from Hospital"
         cursor = self.conexion.cursor()
         cursor.execute(sql)
         datos = []
         for row in cursor:
+            print(row)
+            """
             modelo = model.Hospital()
             modelo.Hospital_cod = row[0]
             modelo.nombre = row[1]
@@ -25,12 +27,13 @@ class ServiceOracleHospital:
             modelo.telefono = row[3]
             modelo.num_cama = row[4]
             datos.append(modelo)
+            """
         cursor.close()
         return datos
 
     def insertarHospital(self,Hospital_cod , nombre , direccion , telefono , num_cama):
-        sql="insert into Hospital values (:Hospital_cod , :nombre , :direccion , :telefono , :num_cama)"
-        cursor = self.conexion.cursor()
+        sql="insert into Hospital values(:Hospital_cod , :nombre , :direccion , :telefono , :num_cama)"
+        cursor=self.conexion.cursor()
         cursor.execute(sql, (Hospital_cod , nombre , direccion , telefono , num_cama))
         registrosAfectados=cursor.rowcount
         self.conexion.commit()
@@ -39,7 +42,7 @@ class ServiceOracleHospital:
         return registrosAfectados
 
     def eliminarHospital(self, idHospital):
-        sql="delete from Hospital where Hospital_no=:idHospital"
+        sql="delete from Hospital where Hospital_cod=:idHospital"
         cursor = self.conexion.cursor()        
         cursor.execute(sql, (idHospital, ))
         registros = cursor.rowcount
@@ -48,13 +51,15 @@ class ServiceOracleHospital:
         print(f"modificados: {registros} registros")
         return registros
 
-    def modificaHospital(self,Hospital_cod , nombre , direccion , telefono , num_cama):
-        sql="update Hospital set( hospital_cod = :inumeroHospital , Hospital_no = :inumeroHospital, apellido = :iapellido,  especialidad = :iespecialidad, salario = :isalario) where Hospital_no = :inumeroHospital"
+    def modificaHospital(self, Hospital_cod , nombre , direccion , telefono , num_cama ):
+        sql="update Hospital set (nombre=:inombre,  direccion=:idireccion, telefono=:itelefono ,num_cama=:inum_cama where Hospital_cod=:Hospital_cod)"
         cursor = self.conexion.cursor()        
-        cursor.execute(sql, (Hospital_cod , nombre , direccion , telefono , num_cama))
+        cursor.execute(sql, (Hospital_cod , nombre , direccion , telefono , num_cama ))
         registros=cursor.rowcount
         self.conexion.commit()
         cursor.close()
         print(f"modificados: {registros} registros")
         return registros
+
+
 
