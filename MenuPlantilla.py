175cc0c7ad43123cc74a092513816498545fe425
DavidB <davidbarroso@telefonica.net>
2025-03-03 10:12:10 +0100

    Update MenuPlantilla.py

--- MenuPlantilla.py
+++ MenuPlantilla.py
@@ -1,1 +1,58 @@
 
+from services import servicioOraclePlantilla as service
+from models import plantilla 
+
+class MENUPLANTILLA:
+        servicio=service.ServiceOraclePlantilla()
+        def mostrarPlantillaes():
+            servicio=service.ServiceOraclePlantilla()
+            afectados=servicio.muestraPlantillaes()
+            for fila in afectados:
+                print(fila)
+                #print(fila.numeroPlantilla, fila.numeroPlantilla, fila.apellido, fila.especialidad, fila.salario)
+
+
+        def nuevoPlantilla():
+            servicio=service.ServiceOraclePlantilla()
+            #afectados=servicio.insertarPlantilla(numeroPlantilla,nombrePlantilla, direccion, telefono, numeroCama)
+            print("dame un numeroPlantilla para el Plantilla nuevo")
+            inumeroPlantilla=int(input())
+            print("dame una nombrePlantilla para el Plantilla nuevo" )
+            inombrePlantilla=input()
+            print("dame una direccion para el Plantilla nuevo" )
+            idireccion=input()
+            print("dame un numeroCama para el Plantilla nuevo" )
+            inumeroCama=int(input())     
+            print("dame un telefono para el Plantilla nuevo" )
+            itelefono=int(input())
+
+            data=servicio.insertarPlantilla(inumeroPlantilla,inombrePlantilla, idireccion, itelefono, inumeroCama)
+
+            print(f"nuevo {data}")
+            return data
+
+        def quitarPlantilla():
+            servicio=service.ServiceOraclePlantilla()
+            print("dame un numeroPlantilla para el Plantilla a eliminar")
+            inumeroPlantilla=int(input())
+            data=servicio.eliminarPlantilla(inumeroPlantilla)
+            
+            print(f"afectados: {data}")
+            return data
+
+        def modificarPlantilla():
+            servicio=service.ServiceOraclePlantilla()
+            print("dame un numero para el inumeroPlantilla a modificar")
+            inumeroPlantilla=int(input())
+            print("dame un inombrePlantilla para el {inumeroPlantilla} a modificar")
+            inombrePlantilla=input()
+            print("dame un  idireccion a modificar")
+            idireccion=input()
+            print("dame un telefono el Plantilla a modificar")
+            itelefono=int(input())
+            print("dame un numero de camas para el Plantilla a modificar")
+            inumeroCama=int(input())
+            data=servicio.modificaPlantilla(inumeroPlantilla, inombrePlantilla, idireccion, itelefono, inumeroCama)
+            print(f"afectados:{data}")
+            return data
+
