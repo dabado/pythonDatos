d27b21191a78013fdf07dce4aca3848abb4ec575
DavidB <davidbarroso@telefonica.net>
2025-03-04 09:59:47 +0100

    Update MenuPlantilla.py

--- MenuPlantilla.py
+++ MenuPlantilla.py
@@ -68,3 +68,26 @@ class MENUPLANTILLA:
             afectados=servicio.buscarOraclePlantilla(hospital_cod)
             print(f"Departamento encontrado: {hospital_cod}, {afectados}")
             return afectados
+
+
+
+        def buscarPlantillaNumero():
+                print("Introduzca número de departamento")
+                data = input()
+                cursor = connection.cursor()
+                sql = "select * from DEPT where DEPT_NO=" + data
+                cursor.execute(sql)
+                #COMO ESTAMOS BUSCANDO POR PK, SOLAMENTE NOS PUEDE 
+                #DEVOLVER UNA FILA
+                row = cursor.fetchone()
+                #DEBEMOS COMPROBAR SI FILA TIENE CONTENIDO/ALGO
+                if (not row):
+                    print("No existe el departamento")
+                else:
+                    #DIBUJAMOS LOS DATOS
+                    nombre = row[1]
+                    localidad = row[2]
+                    print(nombre + ", " + localidad)
+                cursor.close()
+                connection.close()
+                print("Fin de bbdd")
